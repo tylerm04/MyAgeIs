e5cd8310fbd33337fe57dc97eb3e69d5151d7b15
Tyler Mcdowell <tyler.mcdowell@patelhs.org>
2020-09-22 13:55:53 +0000

    Fixed errors and tested if statement.

--- Main.java
+++ Main.java
@@ -1,15 +1,17 @@
-class Main {CheckMyAge
-  public static void main(String[] args) {
+class Main 
+{
+  public static void main(String[] args) 
+  {
     
-    int myAge = 16
-    int myBirthday = 0903
-    int todaysDate = 0921
+    int myAge = 16;
+    int myBirthday = 903;
+    int todaysDate = 921;
    
-   if(myBirthday == 0904)
+   if(myBirthday == todaysDate)
    {
-     System.out.println("It is my birthday.");\
+     System.out.println("It is my birthday.");
    }else{
-     System.out.println("It isn't my birthday.")
+     System.out.println("It isn't my birthday.");
    }
   }
 }
