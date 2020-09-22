463cb70766d0c101cc9cf093fac9e6b5f851afd1
Tyler Mcdowell <tyler.mcdowell@patelhs.org>
2020-09-22 13:50:14 +0000

    I added the int variables and I added an if/then statement and will test what I have so far.

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
-class Main {
+class Main {CheckMyAge
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    
+    int myAge = 16
+    int myBirthday = 0903
+    int todaysDate = 0921
+   
+   if(myBirthday == 0904)
+   {
+     System.out.println("It is my birthday.");\
+   }else{
+     System.out.println("It isn't my birthday.")
+   }
   }
 }
